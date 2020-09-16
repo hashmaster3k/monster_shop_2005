6e25b30a8933dbf648bad6ca625aa1110d014175
Dani Coleman <danicoleman00@gmail.com>
2020-09-16 14:52:42 -0600

    Test that when a merchant is disabled it also disables all items associated with that merchant

--- spec/features/admins/merchants/index_spec.rb
+++ spec/features/admins/merchants/index_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Admins merchant index page' do
   before :each do
-  @print_shop = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
-  @bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203, status: "disabled")
-  @admin = User.create!(name: 'Chilly Billy',
+    @print_shop = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203, status: "disabled")
+    @admin = User.create!(name: 'Chilly Billy',
                         address: '125 Song St.',
                         city: 'Las Vegas',
                         state: 'NV',
@@ -12,6 +12,11 @@ RSpec.describe 'Admins merchant index page' do
                         email: 'chilly_b@admin.com',
                         password: '123',
                         role: 2)
+
+    @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @paper = @print_shop.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+    @pencil = @print_shop.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
     visit '/login'
 
     fill_in :email, with: @admin.email
@@ -57,10 +62,21 @@ RSpec.describe 'Admins merchant index page' do
       expect(page).to have_content("enabled")
     end
   end
+
+  it "I click on the disable button for an enabled merchant, then all of that merchants items should be deactivated" do
+    visit '/admin/merchants'
+
+    within "#merchant-#{@print_shop.id}" do
+      click_button "disable"
+    end
+
+    visit "/items"
+
+    expect(page).to_not have_content(@paper.name)
+    expect(page).to_not have_content(@pencil.name)
+  end
 end
 # As an admin
-# When I visit the admin's merchant index page ('/admin/merchants')
-# I see a "disable" button next to any merchants who are not yet disabled
-# When I click on the "disable" button
-# I am returned to the admin's merchant index page where I see that the merchant's account is now disabled
-# And I see a flash message that the merchant's account is now disabled
+# When I visit the merchant index page
+# And I click on the "disable" button for an enabled merchant
+# Then all of that merchant's items should be deactivated
