83152735f726a84e19bc8086475a59a9dde3356a
Dani Coleman <danicoleman00@gmail.com>
2020-09-16 14:52:14 -0600

    Create a test for disable all items method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -63,5 +63,9 @@ describe Merchant, type: :model do
       expect(@meg.distinct_cities).to include("Hershey")
     end
 
+    it "disable_all_items" do
+      @meg.disable_all_items
+      expect(@tire.active?).to eq(false)
+    end
   end
 end
